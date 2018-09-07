fe23ead5e9d64ab231dcada57ece2130aae6c392
Aeolus1364 <33384516+Aeolus1364@users.noreply.github.com>
2018-09-07 15:02:05 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
 import smbus
 
 class SCD30:
+  ADR = 0x61
+  
+  def init(self, port=1):
+    self.bus = smbus.SMBus(port)
+    
+  def sendCommand(self, cmd):  # sends a 2 byte command
+    bus.write_byte(ADR, cmd >> 8)  # sends the MSB
+    bus.write_byte(ADR, cmd & 0xFF)  # sends the LSB
+  
+  def readRegister(self, reg):
+    bus.write_byte(ADR, reg >> 8)
+    bus.write_byte(ADR, reg & 0xFF)
+    
+    msb = bus.read_byte()
+    lsb = bus.read_byte()
+    data = msb << 8 | lsb
+    
+    return data
