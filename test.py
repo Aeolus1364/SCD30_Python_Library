0cc6d56adfc0441e1cd21d9b571b8b98f12e5741
Aeolus1364 <33384516+Aeolus1364@users.noreply.github.com>
2018-09-07 15:08:44 -0400

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,23 @@ class SCD30:
   def init(self, port=1):
     self.bus = smbus.SMBus(port)
     
-  def sendCommand(self, cmd):  # sends a 2 byte command
-    bus.write_byte(ADR, cmd >> 8)  # sends the MSB
-    bus.write_byte(ADR, cmd & 0xFF)  # sends the LSB
+  def sendCommand(self, cmd, val=None):  # sends a 2 byte command
+    data = []
+    if val:
+      data[0] = val >> 8
+      data[1] = val & 0xFF
+      
+      crc = calcCRC8(data)
+      
+      bus.write_byte(ADR, cmd >> 8)  # sends the MSB
+      bus.write_byte(ADR, cmd & 0xFF)  # sends the LSB
+      bus.write_byte(ADR, data[0])
+      bus.write_byte(ADR, data[1])
+      bus.write_byte(ADR, crc)
+    
+    else:
+      bus.write_byte(ADR, cmd >> 8)  # sends the MSB
+      bus.write_byte(ADR, cmd & 0xFF)  # sends the LSB
   
   def readRegister(self, reg):
     bus.write_byte(ADR, reg >> 8)
@@ -17,5 +31,6 @@ class SCD30:
     msb = bus.read_byte()
     lsb = bus.read_byte()
     data = msb << 8 | lsb
-    
     return data
+  
+  def calcCRC8(data, length):
