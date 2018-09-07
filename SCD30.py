20580962c5268e5dc3cecf8aceb32f972440ea84
Aeolus1364 <33384516+Aeolus1364@users.noreply.github.com>
2018-09-07 16:56:01 -0400

    Changes in the way i2c data is sent

--- SCD30.py
+++ SCD30.py
@@ -5,31 +5,16 @@ class SCD30:
         self.bus = smbus.SMBus(port)
         self.adr = address
 
-    def sendCommand(self, cmd, val=None):  # sends a 2 byte command
-        if val:
-            data = []
-            data[0] = val >> 8
-            data[1] = val & 0xFF
-
-            # crc = calcCRC8(data)
-
-            self.bus.write_byte(self.adr, cmd >> 8)  # sends the MSB
-            self.bus.write_byte(self.adr, cmd & 0xFF)  # sends the LSB
-            self.bus.write_byte(self.adr, data[0])
-            self.bus.write_byte(self.adr, data[1])
-            self.bus.write_byte(self.adr, crc)
-
-        else:
-            self.bus.write_byte(self.adr, cmd >> 8)  # sends the MSB
-            self.bus.write_byte(self.adr, cmd & 0xFF)  # sends the LSB
+    def sendCommand(self, cmd):  # sends a 2 byte command
+        data = []
+        data[0] = cmd >> 8
+        data[1] = cmd & 0xFF
+        self.bus.write_i2c_block_data(self.adr, data[0], data[1:])
 
     def readRegister(self, reg):
-        self.bus.write_byte(self.adr, reg >> 8)
-        self.bus.write_byte(self.adr, reg & 0xFF)
+        self.sendCommand(reg)
 
-        msb = self.bus.read_byte(self.adr)
-        lsb = self.bus.read_byte(self.adr)
-        data = msb << 8 | lsb
+        data = bytearray(read_i2c_block_data(self.adr, 0))
         return data
 
         # def calcCRC8(data, length):
