a191541b08d84958f247da665ab5fd59cfca5a98
Aeolus1364 <33384516+Aeolus1364@users.noreply.github.com>
2018-09-07 16:23:31 -0400

    bus was not referenced as self

--- test.py
+++ test.py
@@ -14,19 +14,19 @@ class SCD30:
 
             # crc = calcCRC8(data)
 
-            bus.write_byte(ADR, cmd >> 8)  # sends the MSB
-            bus.write_byte(ADR, cmd & 0xFF)  # sends the LSB
-            bus.write_byte(ADR, data[0])
-            bus.write_byte(ADR, data[1])
-            bus.write_byte(ADR, crc)
+            self.bus.write_byte(ADR, cmd >> 8)  # sends the MSB
+            self.bus.write_byte(ADR, cmd & 0xFF)  # sends the LSB
+            self.bus.write_byte(ADR, data[0])
+            self.bus.write_byte(ADR, data[1])
+            self.bus.write_byte(ADR, crc)
 
         else:
-            bus.write_byte(ADR, cmd >> 8)  # sends the MSB
-            bus.write_byte(ADR, cmd & 0xFF)  # sends the LSB
+            self.bus.write_byte(ADR, cmd >> 8)  # sends the MSB
+            self.bus.write_byte(ADR, cmd & 0xFF)  # sends the LSB
 
     def readRegister(self, reg):
-        bus.write_byte(ADR, reg >> 8)
-        bus.write_byte(ADR, reg & 0xFF)
+        self.bus.write_byte(ADR, reg >> 8)
+        self.bus.write_byte(ADR, reg & 0xFF)
 
         msb = bus.read_byte()
         lsb = bus.read_byte()
