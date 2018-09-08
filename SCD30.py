dc9bf496698ae89eabf2d106e5991db09af821ca
Aeolus1364 <33384516+Aeolus1364@users.noreply.github.com>
2018-09-08 00:27:31 -0400

    Update to dataReady

--- SCD30.py
+++ SCD30.py
@@ -14,6 +14,8 @@ class Sensor:
 
     def dataReady(self):
         data = self.readRegister(self.COMMAND_DATA_READY, 3)
+        print(data)
+        print(data[0:1])
         ready = data[0] << 8 | data[1]
         crc = self.calcCRC8(data[0:1])
         print(crc, data[2])
